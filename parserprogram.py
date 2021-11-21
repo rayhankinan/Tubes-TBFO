469da1a712645606d0ba10d8a150caba2fcd86e1
rayhankinan <rayhankinan@gmail.com>
2021-11-21 21:57:46 +0700

    Inisialisasi file lagi

---
+++ parserprogram.py
@@ -0,0 +1,5 @@
+from argparse import ArgumentParser
+
+if __name__ == "__name__":
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("nama_file", type=str, help="Nama File yang hendak diparse.")
